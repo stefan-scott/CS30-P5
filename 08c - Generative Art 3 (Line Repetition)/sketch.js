96984ec09c130642939f3b2c4d046ff20cff7d37
stefan-scott <scottst@spsd.sk.ca>
2024-03-25 16:00:37 -0600

    complete demo

--- 08c - Generative Art 3 (Line Repetition)/sketch.js
+++ 08c - Generative Art 3 (Line Repetition)/sketch.js
@@ -2,11 +2,32 @@
 // Mr. Scott
 // March 25, 2024
 
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noLoop(); //just for now
+  background(255);
+  angleMode(DEGREES);
+  stroke(0, 80);
+}
+
+function randomElement(currentLen) {
+  //create one irregular line using
+  //rotate transformations
+  push();  //isolate the coordinate transforms
+  rotate(random(360));
+  while (currentLen > 5) {
+    rotate(random(-40, 40));
+    line(0, 0, 0, currentLen);
+    translate(0, currentLen);
+    currentLen *= 0.75;
+  }
+  pop();
 }
 
 function draw() {
-  background(220);
+  translate(width / 2, height / 2);
+  for (let i = 0; i < 500; i++) {
+    randomElement(random(20,70));
+  }
+
 }
