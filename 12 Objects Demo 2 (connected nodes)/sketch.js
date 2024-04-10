c77c208722c84caa7b9976b7ccf876fab51faf64
stefan-scott <scottst@spsd.sk.ca>
2024-04-10 15:15:14 -0600

    Update sketch.js

--- 12 Objects Demo 2 (connected nodes)/sketch.js
+++ 12 Objects Demo 2 (connected nodes)/sketch.js
@@ -5,6 +5,7 @@
 
 // Global Variables
 let points = [];
+let reach = 150;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -15,6 +16,8 @@ function draw() {
   for(let p of points){ 
     //this style of loop is perfect as long as we don't
     //need to delete any items from the array.
+    p.move();
+    p.connect(points);
     p.display();
   }
 }
@@ -42,7 +45,52 @@ class MovingPoint{
     fill(this.c);
     noStroke();
 
+    //is the mouse close?
+    let d = dist(mouseX, mouseY, this.x, this.y);
+    if(d < reach){
+      this.size = map(d, 0 , reach, 60, 20 );
+    }
+    else{
+      this.size = 20;
+    }
+
     circle(this.x, this.y, this.size);
+  }
+
+  getX() {return this.x;}
+  getY() {return this.y;}
+
+  connect(pointArray){
+    //check if any points are nearby other points, and if
+    //so, join with a line segment
+    stroke(this.c);
+    for(let p of points){
+      //this.x, this.y  p.getX()  p.getY()
+      if(p !== this){
+        let d = dist(this.x, this.y, p.getX(), p.getY());
+        if(d < reach){  //the 2 points are close...
+          line(this.x, this.y, p.getX(), p.getY());
+        }
+      }
+    }
+  }
+
+  move(){
+    //using perlin noise, move horizontally/vertically
+    let xSpeed = noise(this.xTime);  //0-1
+    xSpeed = map(xSpeed, 0, 1, -this.maxSpeed, this.maxSpeed);
+    this.xTime += this.timeShift; //0.01
+
+    this.x += xSpeed;
+
+    let ySpeed = noise(this.yTime);
+    ySpeed = map(ySpeed, 0, 1, -this.maxSpeed, this.maxSpeed);
+    this.yTime += this.timeShift;
+
+    this.y += ySpeed;
+    //add some wrap-around code
+    if(this.x < 0) this.x = width;
+    else if (this.x > width) this.x = 0;
   }
 }
 
