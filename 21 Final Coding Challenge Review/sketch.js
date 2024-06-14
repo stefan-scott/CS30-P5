6f08ec92b144be7ce412b77e1d1259b5b1d3d477
stefan-scott <scottst@spsd.sk.ca>
2024-06-14 14:43:28 -0600

    wip

---
+++ 21 Final Coding Challenge Review/sketch.js
@@ -0,0 +1,39 @@
+// Final Coding Challenge Review
+
+let gorillaIdle = [];
+let goriallaSwipe = [];
+let spiralImages = [];
+
+// Global Variables...
+
+
+function preload(){ //to ensure loading is done
+  //Spirals 00-09   10-15
+  for (let i = 0; i <= 15; i++){
+    if(i < 10){
+      spiralImages.push(loadImage("assets/Circle/Circle Animation0"+i+".png"));
+    }
+    else{
+      spiralImages.push(loadImage("assets/Circle/Circle Animation"+i+".png"));
+    }
+  }
+
+  //Gorialla IDLE images
+  for(let i = 1; i <= 6; i++){ //1, 2, 3, 4, 5, 6
+    gorillaIdle.push(loadImage("idle" + i + ".png"));
+  }
+
+  //Gorialla SWIPE images
+  for(let i = 1; i <= 6; i++){ //1, 2, 3, 4, 5, 6
+    goriallaSwipe.push(loadImage("swipe" + i + ".png"));
+  }
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+}
+
+function draw() {
+ 
+}
